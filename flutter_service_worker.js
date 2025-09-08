a989e45ea55d8037aeb7156c09b49b7502cbf734
cainatoma <cainatomaofficial@gmail.com>
2025-09-08 16:32:05 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,11 +4,11 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"palette.png": "5b3092fa8dc319909ea1ec61666fc21f",
-"flutter_bootstrap.js": "27a7883b58ab691f473732bacc247ba8",
+"flutter_bootstrap.js": "492b306891d6e9f2f67d36904970ddf6",
 "version.json": "45fac50c3fcb39c20f2ec768137ce35d",
-"index.html": "157ea27512e31103317e58ca11055d93",
-"/": "157ea27512e31103317e58ca11055d93",
-"main.dart.js": "6f31d5438efbc9d9b81220050bbe4070",
+"index.html": "96e5dada5d3809e51b621fc7e8e405b6",
+"/": "96e5dada5d3809e51b621fc7e8e405b6",
+"main.dart.js": "1984c5d0c3cc4898bbcd943fd5eb59de",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
